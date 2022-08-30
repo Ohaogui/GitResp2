149099c2f7231f80493a4e72fb773bb1f7114ffe
T <977969930@qq.com>
2022-08-30 12:19:09 +0800

    提交Test.java 文件

---
+++ src/com/msb/Test.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther:Ahao
+ * @Date: 2022/8/30 - 08 - 30 - 12:12
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Test {
+    private Integer age;
+    private String name;
+}
