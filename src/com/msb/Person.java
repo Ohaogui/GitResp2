b2877e7a07ce5269adeecb412d08f3820051e084
T <977969930@qq.com>
2022-08-30 12:38:16 +0800

    提交Person属性文件

---
+++ src/com/msb/Person.java
@@ -0,0 +1,13 @@
+package com.msb;
+
+/**
+ * @Auther:Ahao
+ * @Date: 2022/8/30 - 08 - 30 - 12:36
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private String name;
+    private Integer age;
+    private Integer height;
+}
